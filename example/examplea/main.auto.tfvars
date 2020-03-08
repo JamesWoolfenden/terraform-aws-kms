8f41e1621359ba92d9ebf87f3599d5d36c899c62
James Woolfenden <james.woolfenden@gmail.com>
2020-03-08 12:55:44 +0000

    fix for checkov

--- example/examplea/main.auto.tfvars
+++
@@ -1,4 +0,0 @@
-common_tags = {
-"createdby" = "Terraform" }
-
-accounts = ["111111111111", "222222222222", "680235478471"]
--- example/examplea/variables.tf
+++ example/examplea/variables.tf
@@ -4,14 +4,6 @@ variable "common_tags" {
 }
 
 variable "key" {
-  default = {
-    description              = "AMI-Sharing"
-    deletion_window_in_days  = 30
-    key_usage                = "ENCRYPT_DECRYPT"
-    customer_master_key_spec = "SYMMETRIC_DEFAULT"
-    is_enabled               = true
-    enable_key_rotation      = false
-  }
 }
 
 variable "accounts" {
